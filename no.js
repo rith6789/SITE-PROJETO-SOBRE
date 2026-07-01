64f3102c848a46a4cb1a480cad6198f5648c05e2
rith6789 <capir.2025116isinf0040@aluno.ifpi.edu.br>
2026-07-01 07:09:49 -0300

    Delete no.js

--- no.js
+++
@@ -1,6 +0,0 @@
-function voltar() {
-    window.location.href = "trato.html";
-}
-const nome = localStorage.getItem("Aulejo");
-document.getElementById("boasVindas").textContent =
-`Bem-vindo, ${nome}!`;
